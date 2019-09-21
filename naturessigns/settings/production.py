6ddc29060a249ba7e75fdd0574fbad885c4cecf1
indekx <indekxng@gmail.com>
2019-09-21 08:58:30 +0100

    Email settings added

--- naturessigns/settings/production.py
+++ naturessigns/settings/production.py
@@ -157,12 +157,12 @@ LOGOUT_REDIRECT_URL = 'index'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # EMAIL BACKENDS
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+""" EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_EMAIL_FROM = 'Nature\'s Signs Admin Team <sadmin@naturessigns.com>'
+DEFAULT_EMAIL_FROM = 'Nature\'s Signs Admin Team <admin@naturessigns.com>' """
 
